e3788ccccb2367f5832c3d7f80edf4f8127bd2a8
mohitacharya4 <mohitacharya4@gmail.com>
2023-01-13 00:37:42 +0530

    react.js forms with validation

---
+++ src/components/UI/Button.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import classes from './Button.module.css';
+
+const Button = (props) => {
+  return <button type={props.type || 'button'} className={classes.button} onClick={props.onClick}>{props.children}</button>;
+};
+
+export default Button;
